29464831d018eb7b33cf90d5883f4dd599ca4696
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-09-20 03:10:13 -0400

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -1,5 +1,4 @@
 ---- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. The STATION table is described as follows:
-
 select CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
 select CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, CITY LIMIT 1;
 
@@ -10,4 +9,46 @@ OR
 CITY NOT IN (SELECT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u')
 
 
-----
+---- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+select distinct CITY FROM STATION WHERE CITY NOT IN (SELECT CITY FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%')
+AND
+CITY NOT IN (SELECT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u')
+
+
+---- Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT NAME FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME,3), ID ASC
+
+
+---- Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+---- Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+SELECT CEILING(AVG(SALARY)-AVG(REPLACE(SALARY, '0', ''))) FROM EMPLOYEES
+
+
+---- We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+SELECT MAX(SALARY*MONTHS), COUNT(NAME) FROM EMPLOYEE
+GROUP BY SALARY*MONTHS
+ORDER BY SALARY*MONTHS DESC LIMIT 1
+
+
+---- Consider  and  to be two points on a 2D plane.
+-- happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+-- happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+-- happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+-- happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+SELECT ROUND(ABS(MAX(LAT_N)-MIN(LAT_N))+ABS(MIN(LONG_W)-MAX(LONG_W)),4) FROM STATION
+
+
+---- Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+SELECT ROUND(SQRT((POWER((MAX(LAT_N)-MIN(LAT_N)),2)+POWER((MAX(LONG_W)-MIN(LONG_W)),2))),4) FROM STATION
+
+
+---- 
+
+
+
+
+
+
