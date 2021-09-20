e06840087a46020602c068db6c845cf6fafe9384
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-09-19 23:02:37 -0400

    Update sqlzoo_select_from.sql

--- sqlzoo_select_from.sql
+++ sqlzoo_select_from.sql
@@ -51,5 +51,3 @@ SELECT name, capital
  SELECT name
    FROM world
 WHERE name NOT LIKE '% %' AND name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
-
---14. 
