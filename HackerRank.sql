cf5b10e26115f86307f22cf399c7556c7cd9829c
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-09-23 23:47:48 -0400

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -45,10 +45,12 @@ SELECT ROUND(ABS(MAX(LAT_N)-MIN(LAT_N))+ABS(MIN(LONG_W)-MAX(LONG_W)),4) FROM STA
 SELECT ROUND(SQRT((POWER((MAX(LAT_N)-MIN(LAT_N)),2)+POWER((MAX(LONG_W)-MIN(LONG_W)),2))),4) FROM STATION
 
 
----- 
-
-
-
+---- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+--Equilateral: It's a triangle with  sides of equal length.
+--Isosceles: It's a triangle with  sides of equal length.
+--Scalene: It's a triangle with  sides of differing lengths.
+--Not A Triangle: The given values of A, B, and C don't form a triangle.
+SELECT IF(A+B>C AND B+C>A AND A+C>B, IF(A=B AND B=C,'Equilateral',IF(A=B OR B=C OR A=C, 'Isosceles', 'Scalene')),'Not A Triangle') FROM TRIANGLES
 
 
 
