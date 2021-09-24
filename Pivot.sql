5559fb0693af39c85afd745e3a4b1c3c0f989466
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-09-24 03:32:28 -0400

    Create Pivot.sql

---
+++ Pivot.sql
@@ -0,0 +1,52 @@
+---- Creating Table
+
+CREATE TABLE tblProductSales
+    ([SalesAgent] varchar(50), [SalesCountry] varchar(50), [SalesAmount] int);
+
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES    ('Tom', 'UK', '200');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'US', '180');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'UK', '260');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'India', '450');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('Tom', 'India', '350');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'US', '200');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('Tom', 'US', '130');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'India', '540');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'UK', '120');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'UK', '220');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'UK', '420');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'US', '320');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('Tom', 'US', '340');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('Tom', 'UK', '660');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'India', '430');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'India', '230');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'India', '280');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('Tom', 'UK', '480');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('John', 'US', '360');
+INSERT INTO tblProductSales ([SalesAgent], [SalesCountry], [SalesAmount])
+VALUES ('David', 'UK', '340');
+
+
+
+
+
+
+----
