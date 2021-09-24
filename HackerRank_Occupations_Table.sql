4d27966953694c4b15a2aa46ba865364965370b7
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-09-24 04:34:27 -0400

    Create HackerRank_Occupations_Table.sql

---
+++ HackerRank_Occupations_Table.sql
@@ -0,0 +1,23 @@
+----
+CREATE TABLE OCCUPATIONS   ([Name] varchar(10), [Occupation] varchar(10));
+
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Ashley', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Samantha', 'Actor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Julia', 'Doctor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Britney', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Maria', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Meera', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Priya', 'Doctor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Priyanka', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Jennifer', 'Actor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Ketty', 'Actor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Belvet', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Naomi', 'Professor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Jane', 'Singer')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Jenny', 'Singer')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Kristeen', 'Singer')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Christeen', 'Singer')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Eve', 'Actor')
+INSERT INTO OCCUPATIONS   ([Name], [Occupation]) VALUES    ('Aamina', 'Doctor');
+
+
